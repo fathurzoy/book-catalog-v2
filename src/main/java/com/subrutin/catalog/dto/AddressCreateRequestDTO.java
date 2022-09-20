d7e4eb29e90a0c2263fe68ae1b8ee9089a0a0aa6
Fathur Rahman <fathur.rahman@udana.id>
2022-09-20 14:17:41 +0700

    casecade persist or all

---
+++ src/main/java/com/subrutin/catalog/dto/AddressCreateRequestDTO.java
@@ -0,0 +1,20 @@
+package com.subrutin.catalog.dto;
+
+import java.io.Serializable;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+import lombok.Data;
+
+@Data
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class AddressCreateRequestDTO implements Serializable{
+  
+  private String streetName;
+
+  private String cityName;
+
+  private String zipCode;
+
+}
